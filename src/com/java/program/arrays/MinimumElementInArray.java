f28695869721ae001e89246ffb2cc99f51cc4e98
padmavathi <bpadmavathi0123@gmail.com>
2023-04-18 22:37:15 +0530

    Assignment on Array Programs

---
+++ src/com/java/program/arrays/MinimumElementInArray.java
@@ -0,0 +1,30 @@
+package com.java.program.arrays;
+
+import java.util.Scanner;
+
+public class MinimumElementInArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 1
+		System.out.print("Enter the length of the array : ");
+		int length = sc.nextInt();
+		// 2
+		int[] arr = new int[length];// instantiation,empty array create
+		System.out.print("Enter the array of the elements : ");
+		for (int i = 0; i < length; i++) {
+			arr[i] = sc.nextInt();// initialization,we are loading elements into the array
+		}
+
+		int min = arr[0];
+		// Loop through the array
+		for (int i = 0; i < arr.length; i++) {
+			// Compare elements of array with min
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		// 4
+		System.out.println("The minimum element is " + min);// we are printing min element
+		sc.close();
+	}
+
+}
