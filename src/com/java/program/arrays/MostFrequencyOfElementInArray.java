b9c0a85419456c9843fe7bd99098f45f360a05f8
padmavathi <bpadmavathi0123@gmail.com>
2023-04-20 22:19:49 +0530

    Assignment on Array Programs

---
+++ src/com/java/program/arrays/MostFrequencyOfElementInArray.java
@@ -0,0 +1,33 @@
+package com.java.program.arrays;
+
+import java.util.Scanner;
+
+public class MostFrequencyOfElementInArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the lenth of the array :");
+		int length = sc.nextInt();
+		System.out.print("Enter the number of elements in the array:");
+		int[] arr = new int[length];
+		for (int i = 0; i < length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int maxCount = 0;
+		int count=0;
+		int maxElement = 0;
+		for (int i = 0; i < arr.length; i++) {
+			 count = 1;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] == arr[i]) {
+					count++;
+					if (count > maxCount) {
+						maxElement = arr[j];
+						maxCount = count;
+					}
+				}
+			}
+			System.out.print("Element :" + maxElement + "," + "Occurrences : "+maxCount);
+			sc.close();
+		}
+	}
+}
