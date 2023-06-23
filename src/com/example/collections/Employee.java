021604d26263b586d09ad93401ea7d086453b924
padmavathi <bpadmavathi0123@gmail.com>
2023-06-23 09:04:09 +0530

    code refrectoring

--- src/com/example/collections/Employee.java
+++ src/com/example/collections/Employee.java
@@ -1,19 +1,27 @@
 package com.example.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /*
  * Assignment 1:Create an Employee class with instance variables-employeeid(String)and name(String)two employee 
  * objects are equal if they have same employeeid and name.put the employee objects as keys in hashmap and print them using for loop
  */
+/*
+ * in the employee class add an instance variable salary(float) and implement comparable interface to compare salaries create a arraylist of employees and sort them using collections.sort() method
+ */
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
 	private String empId;
 	private String name;
+	private float salary;
 
-	public Employee(String empId, String name) {
+	public Employee(String empId, String name,float salary) {
 		this.empId = empId;
 		this.name = name;
+		this.salary=salary;
 	}
 
 	@Override
@@ -22,7 +30,7 @@ public class Employee {
 			return true;
 		}
 		Employee emp = (Employee) obj;
-		if (this.empId.equals(emp.empId) && this.name.equals(emp.name)) {
+		if (this.empId.equals(emp.empId) && this.name.equals(emp.name)&&this.salary==(emp.salary)) {
 			return true;
 		} else {
 			return false;
@@ -36,21 +44,42 @@ public class Employee {
 		return result;
 	}
 
+	
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", name=" + name + "]";
+		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + "]";
 	}
+
 	public static void main(String[] args) {
 		HashMap<Employee,Integer>employeeDetails=new HashMap<>();
-		Employee emp1= new Employee("padma","101");
+		Employee emp1= new Employee("padma","101",2523);
 		employeeDetails.put(emp1, 1);
-		Employee emp2=new Employee("niha","101");
+		Employee emp2=new Employee("niha","101",2564);
 		employeeDetails.put(emp2, 2);
 		for(Employee key:employeeDetails.keySet()) {
 			System.out.print(key);
 			System.out.println(employeeDetails.get(key));
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter no of employee salaries:");
+			int size=sc.nextInt();
+			ArrayList<Float>employee=new ArrayList<>();
+			System.out.println("Enter employee salaries:");
+			float salary=sc.nextFloat();
+			for(int i=0;i<size;i++) {
+				employee.add(sc.nextFloat());
+			}
+			System.out.println("Before collections sort employee salaries:"+employee);
+			Collections.sort(employee);
+			System.out.println("After colletion sort employee salaries:"+employee);
 		}
 		
+		
+	}
+
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 
--- src/com/example/collections/StudentDetails.java
+++ src/com/example/collections/StudentDetails.java
@@ -1,6 +1,9 @@
 package com.example.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /*
  * Assignment 2:create a student class with instance variables-student id(String)and name(String) two student
@@ -11,42 +14,62 @@ import java.util.HashMap;
 public class StudentDetails {
 	private String studentId;
 	private String studentName;
-	public StudentDetails(String studentId,String studentName) {
-		this.studentId=studentId;
-		this.studentName=studentName;
+	private double gpa;
+
+	public StudentDetails(String studentId, String studentName, double gpa) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.gpa = gpa;
 	}
+
 	@Override
-		public boolean equals(Object obj) {
-		if(this==obj) {
+	public boolean equals(Object obj) {
+		if (this == obj) {
 			return true;
 		}
-		StudentDetails sudent=(StudentDetails)obj;
-		if(this.equals(sudent.studentId)&&this.studentName.equals(sudent.studentName)) {
+		StudentDetails sudent = (StudentDetails) obj;
+		if (this.equals(sudent.studentId) && this.studentName.equals(sudent.studentName) && this.gpa == (sudent.gpa)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		}
+	}
+
 	@Override
-		public int hashCode() {
-		int result=this.studentName.hashCode();
-		result=result+this.studentId.hashCode();
+	public int hashCode() {
+		int result = this.studentName.hashCode();
+		result = result + this.studentId.hashCode();
 		return result;
-		}
+	}
+
 	@Override
 	public String toString() {
 		return "StudentDetails [studentId=" + studentId + ", studentName=" + studentName + "]";
 	}
+
 	public static void main(String[] args) {
-		HashMap<StudentDetails,Integer>student=new HashMap<>();
-		StudentDetails padma=new StudentDetails("niha","200");
+		HashMap<StudentDetails, Integer> student = new HashMap<>();
+		StudentDetails padma = new StudentDetails("niha", "200", 123.5);
 		student.put(padma, 1);
-		StudentDetails padma1=new StudentDetails("padma","200");
+		StudentDetails padma1 = new StudentDetails("padma", "200", 456.7);
 		student.put(padma1, 2);
-		for(StudentDetails key:student.keySet()) {
+		for (StudentDetails key : student.keySet()) {
 			System.out.print(key + " ");
 			System.out.println(student.get(key));
-		}
-	}
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter no of students gpas: ");
+			int size = scanner.nextInt();
+			ArrayList<Double> students = new ArrayList<>();
+			System.out.println("Enter student gpas: ");
+			double gpas = scanner.nextDouble();
+			for (int i = 0; i < size; i++) {
+				students.add(scanner.nextDouble());
+			}
+			System.out.println("Before collection sort students gpas: " + students);
+			Collections.sort(students);
+			System.out.println("After collection sort students gpas: " + students);
 
+		}
+
+	}
 }
