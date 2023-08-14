0dc8d2fe83574d33955699eb7846bb460843020d
padmavathi <bpadmavathi0123@gmail.com>
2023-08-14 19:52:47 +0530

    Assignment on files

--- src/com/javaExample/io/DescendingFrequencyWordCounter.java
+++ src/com/javaExample/io/DescendingFrequencyWordCounter.java
@@ -12,6 +12,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Map.Entry;
 import java.util.Set;
 
@@ -20,13 +21,28 @@ public class DescendingFrequencyWordCounter {
     public static void main(String[] args) {
         String filePath = "D:\\poem.txt";
         String outputFilePath = "D:\\output.txt";
-        //countLines(filePath)
-        //findAndReplace(String actual,String replace)
-        //longestWord(filePath)
-        //writeReverse(eachWordFreqMap,outputFilePath)
+        Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter actual string: ");
+    	String actual = sc.next();
+    	System.out.print("Enter the string to replace : ");
+    	String replace = sc.next();
+        //countLines(String filePath)
+        //findAndReplace(String actual,String replace,String filePath)
+        //longestWord(String filePath)
+        //writeReverse(Map<String,Integer> eachWordFreqMap,outputFilePath)
         Map<String, Integer> eachWordFreqMap = countWordOccurrences(filePath);
         printDescendingFrequency(eachWordFreqMap);
         writeWordFrequency(eachWordFreqMap, outputFilePath);
+        countLines(filePath);
+        findAndReplace(actual,replace,filePath);
+    	String longest=longestWord(filePath);
+    	System.out.println("The longestWord: "+longest);
+    	 writeReverse(eachWordFreqMap, outputFilePath);
+         System.out.println("Reversed content written to the output file.");
+         reverseFileLines(filePath, outputFilePath);
+         System.out.println("File lines reversed successfully.");
+         sc.close();
+        
     }
 
     private static Map<String, Integer> countWordOccurrences(String filePath) {
@@ -86,4 +102,115 @@ public class DescendingFrequencyWordCounter {
 		}
 
 	}
+	
+	private static void countLines(String filePath) {
+		int lineCount = 0;
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			while (reader.readLine() != null) {
+				lineCount++;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Line count: " + lineCount);
+	}
+	
+	
+	private static void findAndReplace(String actual, String replace, String filePath) {
+		StringBuilder content = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				content.append(line).append(System.lineSeparator());
+			}
+			String updatedContent = content.toString().replace(actual, replace);
+
+			try (FileWriter writer = new FileWriter(filePath)) {
+				writer.write(updatedContent);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+	
+	private static String longestWord(String filePath) {
+		String longestWord = "";
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] words = line.split("\\s+");
+				for (String word : words) {
+					if (word.length() > longestWord.length()) {
+						longestWord = word;
+					}
+				}
+			}
+
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return longestWord;
+
+	}
+	
+	 private static void writeReverse(Map<String, Integer> eachWordFreqMap, String outputFilePath) {
+	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+	            for (Map.Entry<String, Integer> entry : eachWordFreqMap.entrySet()) {
+	                String reversedKey = reverseString(entry.getKey());
+	                String reversedLine = reversedKey + " " + entry.getValue(); 
+	                writer.write(reversedLine);
+	                writer.newLine();
+	            }
+	        } 
+	        
+	        catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+
+	    private static String reverseString(String str) {
+	        StringBuilder reversed = new StringBuilder(str);
+	        return reversed.reverse().toString();
+	    }
+	    
+	    private static void reverseFileLines(String filePath, String outputFilePath) {
+			List<String> lines = new ArrayList<>();
+			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+				String line;
+				try {
+					while ((line = reader.readLine()) != null) {
+						lines.add(line);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Collections.reverse(lines);
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+				for (String line : lines) {
+					writer.write(line);
+					writer.newLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	    
+
 }
