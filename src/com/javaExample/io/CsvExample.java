16a2195c85dd1316e405bf69869a9c6407c11691
padmavathi <bpadmavathi0123@gmail.com>
2023-08-16 18:01:51 +0530

    code refactoring

--- src/com/javaExample/io/CsvExample.java
+++ src/com/javaExample/io/CsvExample.java
@@ -1,11 +1,13 @@
 package com.javaExample.io;
 
-import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -16,27 +18,30 @@ import com.opencsv.exceptions.CsvValidationException;
 public class CsvExample {
 	public static void main(String[] args) {
 		String filePath = "C:\\Users\\DELL\\Documents\\input.csv";
-		String name = "padma";
-		int age = 28;
-		String location = "Hyderabad";
-		Scanner sc = new Scanner(System.in);
+		String name = "Padma";
+        int age = 29;
+        String location = "Hyderabad";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a word:");
+        String wordInCount=sc.next();
 		System.out.print("Enter actual string: ");
-    	String actual = sc.next();
-    	System.out.print("Enter the string to replace : ");
-    	String replace = sc.next();
-        
-		Map<String, Integer> eachWordFreqMap = new HashMap<String,Integer>();
-		
+		String actual = sc.next();
+		System.out.print("Enter the string to replace : ");
+		String replace = sc.next();
+         
+	
+
 		// readCsv(String filePath);
 		// writeCsv(String filePath, String name, int age, String location);
 		// countOccurences(String filePath, String word);
 		// findAndReplace(String filePath, String actual, String replace);
 		// countLines(String filePath);
 		readCSV(filePath);
-		writeCsv(filePath,name,age,location);
-		countOccurences(filePath, eachWordFreqMap);
-		wordFrequency(eachWordFreqMap);
-		findAndReplace(actual,replace,filePath);
+		writeCsv(filePath, name, age, location);
+        int wordCount = countOccurrences(filePath, wordInCount);
+        System.out.println("The word '" + wordInCount + "' appears " + wordCount + " times.");
+		findAndReplace(actual, replace, filePath);
+		System.out.println("Word replaced successfully.");
 		countLines(filePath);
 		sc.close();
 
@@ -63,84 +68,101 @@ public class CsvExample {
 		}
 
 	}
-	 private static void countOccurences(String filePath, Map<String, Integer> eachWordFreqMap) {
-	        
-		 try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-	            String line;
-	            while ((line = br.readLine()) != null) {
-	                String[] words = line.split(","); // CSV files are comma-separated
-	                for (String word : words) {
-	                    word = word.toLowerCase().trim();
-	                    if (!word.isEmpty()) {
-	                        int count = eachWordFreqMap.getOrDefault(word, 0) + 1;
-	                        eachWordFreqMap.put(word, count);
-	                    }
-	                }
-	            }
-	        } catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} 
-	    }
-	 
-	 private static void wordFrequency(Map<String, Integer> eachWordFreqMap) {
-	        Scanner sc = new Scanner(System.in);
-	        System.out.println("Enter a word:");
-	        String word = sc.next().toLowerCase();
+	
+	private static void writeCsv(String filePath, String name, int age, String location) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath, true))) {
+            String[] record = {name, String.valueOf(age), location};
+            writer.writeNext(record);
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
 
-	        if (!word.isEmpty()) {
-	            int frequency = eachWordFreqMap.getOrDefault(word, 0);
-	            System.out.println("Frequency of word '" + word + "': " + frequency);
-	        } else {
-	            System.out.println("Invalid input");
-	        }
+	private static int countOccurrences(String filePath, String wordToCount) {
+		int totalCount = 0;
 
-	        sc.close();
-	    }
-	 
-	 private static void findAndReplace(String actual, String replace, String filePath) {
-			StringBuilder content = new StringBuilder();
-			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-				String line;
-				while ((line = reader.readLine()) != null) {
-					content.append(line).append(System.lineSeparator());
+		try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+			String[] nextLine;
+			try {
+				while ((nextLine = reader.readNext()) != null) {
+					for (String cell : nextLine) {
+						totalCount += countOccurrencesInString(cell, wordToCount);
+					}
 				}
-				String updatedContent = content.toString().replace(actual, replace);
+			} catch (CsvValidationException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-				try (FileWriter writer = new FileWriter(filePath)) {
-					writer.write(updatedContent);
+		return totalCount;
+	}
+
+	private static int countOccurrencesInString(String text, String word) {
+		int count = 0;
+		int index = text.indexOf(word);
+		while (index != -1) {
+			count++;
+			index = text.indexOf(word, index + 1);
+		}
+		return count;
+	}
+	
+	
+	private static void findAndReplace(String filePath, String actualWord, String replaceWord) {
+		List<String[]> newLines = new ArrayList<>();
+
+		try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+			String[] nextLine;
+			try {
+				while ((nextLine = reader.readNext()) != null) {
+					String[] newLine = new String[nextLine.length];
+					for (int i = 0; i < nextLine.length; i++) {
+						newLine[i] = nextLine[i].replace(actualWord, replaceWord);
+					}
+					newLines.add(newLine);
 				}
-			} catch (FileNotFoundException e) {
+			} catch (CsvValidationException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 
-	    }
-	 
-	 private static void countLines(String filePath) {
-			int lineCount = 0;
-			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-				while (reader.readLine() != null) {
+		try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
+			for (String[] line : newLines) {
+				writer.writeNext(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private static void countLines(String filePath) {
+		int lineCount = 0;
+		try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+			try {
+				while (reader.readNext() != null) {
 					lineCount++;
 				}
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
+			} catch (CsvValidationException e) {
 				e.printStackTrace();
 			}
-			System.out.println("Line count: " + lineCount);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		System.out.println("Line count: " + lineCount);
+	}
 
-		private static void writeCsv(String filePath, String name, int age, String location) {
-			try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
-				String[] record = { name, Integer.toString(age), location };
-				writer.writeNext(record);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	
 
-
+	
 }
