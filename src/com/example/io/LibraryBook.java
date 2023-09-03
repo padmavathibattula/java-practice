1b6083d6b69eb7d45c9453df182a20290a1b5646
padmavathi <bpadmavathi0123@gmail.com>
2023-09-03 10:42:47 +0530

    Assignment on convertXmlJsonGeneration

---
+++ src/com/example/io/LibraryBook.java
@@ -0,0 +1,20 @@
+package com.example.io;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement(name = "books")
+public class LibraryBook {
+	 private List<LibraryBookDetails> books;
+
+	    public LibraryBook() {
+	        books = new ArrayList<>();
+	    }
+
+	    @XmlElement(name = "book")
+	    public List<LibraryBookDetails> getBooks() {
+	        return books;
+	    }
+
+}
---
+++ src/com/example/io/LibraryBookDetails.java
@@ -0,0 +1,57 @@
+package com.example.io;
+
+public class LibraryBookDetails {
+	 private int bookId;
+	    private String title;
+	    private String author;
+	    private String publicationYear;
+	    private String isbn;
+	    private String genre;
+	    private String copiesAvailable;
+		public int getBookId() {
+			return bookId;
+		}
+		public void setBookId(int bookId) {
+			this.bookId = bookId;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		public String getPublicationYear() {
+			return publicationYear;
+		}
+		public void setPublicationYear(String publicationYear) {
+			this.publicationYear = publicationYear;
+		}
+		public String getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+		public String getGenre() {
+			return genre;
+		}
+		public void setGenre(String genre) {
+			this.genre = genre;
+		}
+		public String getCopiesAvailable() {
+			return copiesAvailable;
+		}
+		public void setCopiesAvailable(String copiesAvailable) {
+			this.copiesAvailable = copiesAvailable;
+		}
+
+	   
+	  
+
+}
